aecc99c17f0607ad9c1abc3935c94321aae1d67f
unknown <atdhe.muharremi@gmail.com>
2023-03-28 01:46:05 +0200

    some changes for app.js

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.use(bodyParser.json());
 app.get('/story', (req, res) => {
   fs.readFile(filePath, (err, data) => {
     if (err) {
-      console.error(err); // log the error to console
       return res.status(500).json({ message: 'Failed to open file.'});
     }
     res.status(200).json({ story: data.toString() });
@@ -22,22 +21,15 @@ app.get('/story', (req, res) => {
 
 app.post('/story', (req, res) => {
   const newText = req.body.text;
-  if (!newText || newText.trim().length === 0) {
+  if (newText.trim().length === 0) {
     return res.status(422).json({ message: 'Text must not be empty!' });
   }
   fs.appendFile(filePath, newText + '\n', (err) => {
     if (err) {
-      console.error(err); // log the error to console
       return res.status(500).json({ message: 'Storing the text failed.'});
     }
     res.status(201).json({ message: 'Text was stored!' });
   });
 });
 
-app.get('/error', (req, res) => {
-  process.exit(1);
-});
-
-app.listen(3000, () => {
-  console.log('Server listening on port 3000.');
-});
+app.listen(3000);
