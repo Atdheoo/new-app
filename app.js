0d5cd190b1d45975a0736f57ab1542b57ddfe0dd
unknown <atdhe.muharremi@gmail.com>
2023-03-28 02:31:39 +0200

    Changes to app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const filePath = path.join(__dirname, 'story', 'text.txt');
+const filePath = path.resolve(__dirname, 'story', 'text.txt');
 
 app.use(bodyParser.json());
 
